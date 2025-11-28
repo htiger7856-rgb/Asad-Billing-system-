5dd5816e0aa9ab633639686785aabd8d45f420fa
htiger7856-rgb <htiger7856@gmail.com>
2025-11-28 19:33:44 +0500

    Refactor service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,13 @@
-// Roman Urdu comment: Yeh service worker offline caching ke liye hai
-
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('billing-cache').then(cache => {
-      return cache.addAll(['/', '/index.html', '/app.js', '/manifest.json']);
+    caches.open("invoice-cache").then(cache => {
+      return cache.addAll(["./", "index.html", "manifest.json"]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
