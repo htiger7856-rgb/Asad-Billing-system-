874b3db11140d74ceb302677bb7c7fd50735f629
htiger7856-rgb <htiger7856@gmail.com>
2025-11-28 20:26:37 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("invoice-cache").then(cache => {
-      return cache.addAll(["./", "index.html", "manifest.json"]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener('install',()=>self.skipWaiting());
